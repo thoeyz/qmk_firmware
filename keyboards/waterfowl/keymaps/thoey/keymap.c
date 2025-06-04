993d5d66c5f0b647152d2c36212b02a8a5dae4e6
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-06-04 10:52:10 +0000

    add combo game toggle default layer

--- keyboards/waterfowl/keymaps/thoey/keymap.c
+++ keyboards/waterfowl/keymaps/thoey/keymap.c
@@ -37,6 +37,7 @@ const uint16_t PROGMEM gamectl[] = {KC_X, KC_LALT, COMBO_END};
 const uint16_t PROGMEM gameb[] = {KC_LALT, KC_E, COMBO_END};
 const uint16_t PROGMEM gameo[] = {KC_ESC, KC_LSFT, COMBO_END};
 const uint16_t PROGMEM gameq[] = {KC_X, KC_3, COMBO_END};
+const uint16_t PROGMEM gametog1[] = {KC_ESC, KC_LSFT, KC_W, KC_C, COMBO_END};
 
 
 combo_t key_combos[] = {
@@ -55,6 +56,7 @@ combo_t key_combos[] = {
     COMBO(gameb, KC_B),
     COMBO(gameo, KC_O),
     COMBO(gameq, KC_Q),
+    COMBO(gametog1, TG(1)),
 };
 
 
