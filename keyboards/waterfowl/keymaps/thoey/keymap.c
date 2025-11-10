276cf6375a470faea76c2347446e9e40b553e536
thoeyz <149983252+thoeyz@users.noreply.github.com>
2025-11-10 11:39:31 +0000

    keymap changes

--- keyboards/waterfowl/keymaps/thoey/keymap.c
+++ keyboards/waterfowl/keymaps/thoey/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [1] = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),ENCODER_CCW_CW(KC_F2, KC_I),			                ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [2] = { ENCODER_CCW_CW(KC_2, KC_1),ENCODER_CCW_CW(C(KC_Z), C(KC_Y)),			                ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [3] = { ENCODER_CCW_CW(KC_5, KC_6),ENCODER_CCW_CW(KC_7, KC_8),				                    ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [4] = { ENCODER_CCW_CW(KC_5, KC_6),ENCODER_CCW_CW(KC_7, KC_8),				                    ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [4] = { ENCODER_CCW_CW(C(KC_PGUP), C(KC_PGDN)),ENCODER_CCW_CW(KC_7, KC_8),				        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [5] = { ENCODER_CCW_CW(KC_5, KC_6),ENCODER_CCW_CW(KC_7, KC_8),				                    ENCODER_CCW_CW(KC_VOLD, KC_VOLU),ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
 };
 #endif
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_Q,		KC_W,	   KC_E,		           KC_R,		           KC_T,				        KC_Y,	KC_U,		        KC_I,	   	   KC_O,	KC_P,
 	LT(3,KC_A),	KC_S,	   KC_D,	  	           LSFT_T(KC_F),	       KC_G,				        KC_H,	LSFT_T(KC_J),       KC_K,	   	   KC_L,	LT(3,KC_SCLN),
 	LT(4,KC_Z),	KC_X,	   KC_C,		           KC_V,		           KC_B,				        KC_N,	KC_M,		        KC_COMM,  	   KC_DOT,	LT(4,KC_SLSH),
-			    TG(1),	   LALT_T(KC_TAB),         LCTL_T(KC_BSPC),        MO(2),  	 KC_LGUI, KC_NUM,	MO(2),	KC_SPC,		        LT(1,KC_ENT),  TG(1)
+			    TG(1),	   LALT_T(KC_TAB),         LCTL_T(KC_BSPC),        MO(2),  	 KC_LGUI, KC_NUM,	MO(2),	KC_SPC,		        LT(1,KC_ENT),  KC_MUTE
 ),
 
 
